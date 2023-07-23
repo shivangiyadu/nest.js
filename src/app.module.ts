00db2075da3e3505ef3fed303b3d2292f7b460e8
stevecodes23 <stevemsih2308@gmail.com>
2023-07-23 17:56:18 +0530

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from "@nestjs/common";
-import { UsersController } from "./users.controllers";
 import { userModule } from "./user/user.module";
 import { user1Module } from "./dto/user1/user1.module";
 import { user2Module } from "./user2/user2.module";
@@ -8,7 +7,7 @@ import { user4Module } from "./user4/user4.module";
 
 @Module({
   imports:[userModule,user1Module,user2Module,user3Module,user4Module],
-  controllers: [UsersController],
+ 
  
 })
 export class AppModule {}
